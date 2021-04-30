6a6e2ff4e8bb3df5f517179b3a8de4666dc25a0d
najim-el-guennouni <najimelguennouni@gmail.com>
2021-04-30 00:19:16 +0000

    Add User/Admin Permissions

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -29,8 +30,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-         yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
-          yield MenuItem::linkToCrud('Product', 'fas fa-clipboard-list', Product::class);
+        yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Product', 'fas fa-clipboard-list', Product::class);
+        if($this->isGranted('ROLE_ADMIN')){
+            yield MenuItem::linkToCrud('User', 'fas fa-user', User::class);
+        }
     }
     
 }
---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use phpDocumentor\Reflection\DocBlock\Tags\Property;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setEntityPermission('ROLE_ADMIN');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new( propertyName: 'email'),
+            TextField::new( propertyName: 'password'),
+            ArrayField::new( propertyName: 'roles'),
+        ];
+    }
+
+}
